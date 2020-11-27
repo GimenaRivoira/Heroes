1ce440f8d8c9de4f28b60aff00e6cd31d11a0149
GimenaRivoira <gimenarivoira@gmail.com>
2020-11-27 11:56:47 -0300

    agregue heroes detalle en app

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.listen(3030, () => console.log('Server running in 3030 port'));
 const rutaMain = ('./routes/main.js')
 const rutaHeroes = ('/routes/heroes.js')
 
+app.use ('/heroes/detalle/:id', rutaHeroes)
+
 /*/ Ruta Raíz / ➝ Home
 app.get('/', function(req,res){
 	res.send('Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en 	ti!.')
