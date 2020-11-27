14770125d2c50f5fcc15a63fa36e2780382d79b8
GimenaRivoira <gimenarivoira@gmail.com>
2020-11-27 12:02:11 -0300

    agregue ruta principal a app

--- app.js
+++ app.js
@@ -13,14 +13,8 @@ app.listen(3030, () => console.log('Server running in 3030 port'));
 const rutaMain = ('./routes/main.js')
 const rutaHeroes = ('/routes/heroes.js')
 
+app.use ('/', rutaMain)
 app.use ('/heroes/detalle/:id', rutaHeroes)
-
-/*/ Ruta Raíz / ➝ Home
-app.get('/', function(req,res){
-	res.send('Ni Superman, Iron Man o La Mujer Maravilla son tan importantes cómo las y los Heroes de carne y hueso que encontrarás en este sitio. Esperamos que ellas y ellos te sirvan como inspiración para poder cumplir tus objetivos. Recuerda: ¡nunca pares de creer en 	ti!.')
-});/** */
-
-//ruta heroes poner lo que falta aca va un export o algo parecido
 
 // Ruta Créditos
 app.get('/creditos', function(req, res){
