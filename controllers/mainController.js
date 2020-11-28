f788cc3822ec1ab69fed1cde9fe74b8e64cfc8bb
GimenaRivoira <gimenarivoira@gmail.com>
2020-11-28 15:59:35 -0300

    integrantes del grupo

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,7 +4,7 @@ module.exports = {
         res.send('Ni Superman,Iron Man o la Mujer Maravilla son tan importantes cómo las y los Heros de carne y hueso que encontrarásen este sitio.Espramos que ellas y ells te sirvan como inspitación para poder cumplir tus objetivos.Recuerda:¡nunca pares de creer en ti!')
     },
     creditos:(req,res)=> {
-        res.send('Autor: Josue Abi Rached')//tienen que cambiar el autor
+        res.send('Autor: Josue Abi Rached, Matias Cardillo, Gimena Rivoira')//tienen que cambiar el autor
     }
 
 
